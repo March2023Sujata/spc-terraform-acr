6587cbe496abd266d4c32bba341e5edcc129ea56
Sujata Joshi <joshi.sujata.v@gmail.com>
2023-05-26 09:00:14 +0100

    update terraform

--- provider.tf
+++ provider.tf
@@ -22,10 +22,10 @@ module "aks" {
   source = "git::https://github.com/Sujata-Joshi/terraform-AKS-Modules.git//modules/aks?ref=main"
   aks_info = {
     rg_name           = "AKS-RG"
-    rg_location       = "eastus"
+    rg_location       = "ukwest"
     aks_name          = "aks-cluster"
-    node_pool_name    = "node1"
-    node_count        = 2
+    node_pool_name    = "node"
+    node_count        = 1
     node_pool_vm_size = "Standard_D2_v2"
     identity-type     = "SystemAssigned"
   }
